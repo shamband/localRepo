c072846973c21fd681d7ddaeef21e7685c52f8b7
Sham Band <130472842+shamband@users.noreply.github.com>
2023-12-19 16:21:19 +0530

    Create cgpaCalculator.js

---
+++ cgpaCalculator.js
@@ -0,0 +1,32 @@
+function calculateCGPA() {
+  const sgpaInputs = document.getElementsByClassName('sgpa');
+  let totalSGPA = 0;
+
+  for (let i = 0; i < sgpaInputs.length; i++) {
+    const sgpaValue = sgpaInputs[i].value.trim();
+    if (sgpaValue !== '') {
+      const sgpa = parseFloat(sgpaValue);
+      if (!isNaN(sgpa)) {
+        totalSGPA += sgpa;
+      } else {
+        alert('Please enter valid numeric values for SGPA.');
+        return;
+      }
+    } else {
+      alert('Please enter SGPA for all semesters.');
+      return;
+    }
+  }
+
+  const numberOfSemesters = sgpaInputs.length;
+  const cgpa = totalSGPA / numberOfSemesters;
+
+  document.getElementById('cgpaResult').innerHTML = `CGPA: ${cgpa.toFixed(2)}`;
+
+  convertToPercentage(cgpa);
+}
+
+function convertToPercentage(cgpa) {
+  const percentage = (cgpa / 10) * 100;
+  document.getElementById('percentageResult').innerHTML = `Percentage: ${percentage.toFixed(2)}%`;
+}
